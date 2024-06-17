f25716c5864a7b8975deb96db603cdc0279153fd
Leonardo FRONT-END <leonardosantoss204@gmail.com>
2024-06-17 15:00:49 -0300

    Feat: adicionando o componente reverso js

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import Welcome from './compoments/Welcome/Welcome';
-import Reverse from './compoments/reverse';
+import Reverse from './compoments/Reverse/Reverse';
 
 function App() {
   return (
     <div className="App">
       <Welcome nome ="Leonardo" sobrenome="Ramos"/>
-      {/* <Reverse texto="ola"/> */}
+      <div className="reverse">
+        <p>Os textos a baixo são criados pelo componente Reverse:</p>
+        <Reverse>
+          meu texto
+        </Reverse>
+        <Reverse>
+          o meu texto que deve ser investido
+        </Reverse>
+      </div>
+      
     </div>
   );
 }
