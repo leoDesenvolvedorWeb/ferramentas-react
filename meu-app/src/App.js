9cd2e77bcd863a1d73f1dd31c1161333e7070b4b
Leonardo FRONT-END <leonardosantoss204@gmail.com>
2024-06-18 15:53:23 -0300

    Fix: O componente Reverse está esperando uma prop texto, mas no App.js, ele está sendo usado como um children

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -5,17 +5,12 @@ import Reverse from './compoments/Reverse/Reverse';
 function App() {
   return (
     <div className="App">
-      <Welcome nome ="Leonardo" sobrenome="Ramos"/>
+      <Welcome nome="Leonardo" sobrenome="Ramos" />
       <div className="reverse">
         <p>Os textos abaixo são criados pelo componente Reverse:</p>
-        <Reverse>
-          meu texto
-        </Reverse>
-        <Reverse>
-          o meu texto que deve ser investido
-        </Reverse>
+        <Reverse texto="meu texto" />
+        <Reverse texto="o meu texto que deve ser investido" />
       </div>
-      
     </div>
   );
 }
