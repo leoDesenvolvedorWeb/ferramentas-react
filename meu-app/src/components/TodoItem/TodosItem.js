54a1a5cfc13fa0eec3996993ed6167e11421f6f8
Leonardo FRONT-END <leonardosantoss204@gmail.com>
2024-06-19 16:37:30 -0300

    Feat: adicionando pastas TodosItem.css TodosItem.js , funcionalidade e atualizando Todolist

---
+++ meu-app/src/components/TodoItem/TodosItem.js
@@ -0,0 +1,10 @@
+const TodoItem = ({ tarefa }) => {
+    return (
+        <li className="todo-item">
+            <input type ="checkbox"/>
+            { tarefa }
+        </li>
+    )
+}
+
+export default TodoItem
--- meu-app/src/components/TodoList/Todolist.js
+++ meu-app/src/components/TodoList/Todolist.js
@@ -1,3 +1,4 @@
+import TodoItem from '../TodoItem/TodosItem';
 import './TodoList.css'
 
 
@@ -6,15 +7,9 @@ const TodoList = () => {
         <section className="todolist">
             <h2 className="todo-title">Minha Lista de Tarefas</h2>
             <ol className="todos">
-                <li className="todo-item">
-                    <input type="checkbox"/>
-                    Ir ao mercado</li>
-                <li className ="todo-item">
-                    <input type="checkbox"/>
-                    Estudar 30 min de javascript</li>
-                <li className ="todo-item">
-                    <input type="checkbox"/>
-                    Meditar 30min</li>
+                <TodoItem tarefa="ir ao mercado"/>
+                <TodoItem tarefa="Ler um livro"/>
+                <TodoItem tarefa="Meditar por 30min"/>
             </ol>
         </section>
     )
