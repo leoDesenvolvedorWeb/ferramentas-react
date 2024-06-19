0b686bf1f2f4f660cb8396b8ec1b792d8a325bad
Leonardo FRONT-END <leonardosantoss204@gmail.com>
2024-06-19 10:34:30 -0300

    Feat: criando o corpo Todolist.js e importando ele no app.js

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Welcome from './compoments/Welcome/Welcome';
 import Reverse from './compoments/Reverse/Reverse';
+import Todolist from './compoments/TodoList/Todolist';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Reverse texto="meu texto" />
         <Reverse texto="o meu texto que deve ser investido" />
       </div>
+      <Todolist/>
     </div>
   );
 }
---
+++ meu-app/src/compoments/TodoList/Todolist.js
@@ -0,0 +1,7 @@
+const Todolist = () => {
+    return (
+        <p>Meu componente Todo list</p>
+    )
+}
+
+export default Todolist;
