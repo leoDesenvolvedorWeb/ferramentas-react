4fbd9a19202422277f472b95bbdd235662e1b56d
Leonardo FRONT-END <leonardosantoss204@gmail.com>
2024-06-21 08:58:11 -0300

    Feat: apredendo a melhorar codigos para proatividade

--- meu-app/src/components/TodoList/Todolist.js
+++ meu-app/src/components/TodoList/Todolist.js
@@ -3,13 +3,14 @@ import './TodoList.css'
 
 
 const TodoList = () => {
+    const tarefa =['Ir ao mercado', 'estudar javascript', 'estudar react', 'lavar a louça']
     return (
         <section className="todolist">
             <h2 className="todo-title">Minha Lista de Tarefas</h2>
             <ol className="todos">
-                <TodoItem tarefa="ir ao mercado"/>
-                <TodoItem tarefa="Ler um livro"/>
-                <TodoItem tarefa="Meditar por 30min"/>
+                {tarefa.map((tarefa, index) => (
+                    <TodoItem tarefa={tarefa} key={index}/>
+                ))}
             </ol>
         </section>
     )
