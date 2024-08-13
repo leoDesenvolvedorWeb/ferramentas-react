1107a19c6e085de9f956fce41ffcfbd2dd011165
Leonardo FRONT-END <leonardosantoss204@gmail.com>
2024-08-13 06:27:21 -0300

    r

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Welcome from './components/Welcome/Welcome';
 import Reverse from './components/Reverse/Reverse';
 import Todolist from './components/TodoList/Todolist';
+import Menssagen from './components/Menssagen/Menssagen';
 
 function App() {
   return (
     <div className="App">
       <Welcome nome="Leonardo" sobrenome="Ramos" />
+      <Menssagen/>
       <div className="reverse">
         <p className="app-text">Os textos abaixo são criados pelo componente Reverse:</p>
         <Reverse texto="meu texto" />
---
+++ meu-app/src/components/Menssagen/Menssagen.js
@@ -0,0 +1,8 @@
+function Menssagen() {
+    // const { menssagen para loja };
+    return(
+        <h1>Venha converir nossas novidades !</h1>
+    )
+}
+
+export default Menssagen;
